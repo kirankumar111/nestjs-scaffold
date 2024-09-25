1490a8a8d93672e6f85ff3ddd03bf0c95ab8f20e
Kiran Kumar <kiranstar2007@gmail.com>
2024-09-25 11:43:37 +0530

    Added a few comments in main.ts Release version: v1.0.2

--- src/configs/kafka.config.ts
+++ src/configs/kafka.config.ts
@@ -1,21 +1,36 @@
 import { registerAs } from '@nestjs/config';
 import { logLevel } from 'kafkajs';
 
-
+// This configuration is used to configure the Kafka client by Confluent Cloud.
 export default registerAs('kafkaConfig', () => ({
+  // Array of Kafka broker addresses
   brokers: [process.env.KAFKA_BROKER],
+  
+  // SSL configuration for secure connections
   ssl: process.env.KAFKA_SSL,
+  
+  // SASL authentication configuration
   sasl: {
-    mechanism: process.env.KAFKA_SASL_MECHANISM,
+    mechanism: process.env.KAFKA_SASL_MECHANISM, // e.g., 'plain', 'scram-sha-256', etc.
     username: process.env.KAFKA_SASL_USERNAME,
     password: process.env.KAFKA_SASL_PASSWORD,
   },
+  
+  // Unique identifier for this Kafka client
   clientId: process.env.KAFKA_CLIENT_ID,
+  
+  // Maximum time in milliseconds to wait for a successful connection
   connectionTimeout: parseInt(process.env.KAFKA_CONNECTION_TIMEOUT),
+  
+  // Maximum time in milliseconds to wait for a request to be acknowledged
   requestTimeout: parseInt(process.env.KAFKA_REQUEST_TIMEOUT),
+  
+  // Retry configuration for failed requests
   retry: {
-    initialRetryTime: process.env.KAFKA_RETRY_INITIAL_RETRY_TIME,
-    retries: process.env.KAFKA_RETRY_RETRIES,
+    initialRetryTime: process.env.KAFKA_RETRY_INITIAL_RETRY_TIME, // Initial backoff time in milliseconds
+    retries: process.env.KAFKA_RETRY_RETRIES, // Maximum number of retries
   },
+  
+  // Set the logging level for the Kafka client (ERROR level in this case)
   logLevel: logLevel.ERROR,
 }));
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { fastifyCookie } from '@fastify/cookie';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
@@ -9,51 +8,64 @@ import { ConfigService } from '@nestjs/config';
 import { ServerConfig, ServerConfigName } from './configs/server.config';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
-import compress from '@fastify/compress';  // Use the correct package
+import compress from '@fastify/compress';
 import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 import { setupSwagger } from './swagger/swagger.config';
 
-
 async function bootstrap() {
+  // Create a NestJS application with Fastify as the underlying HTTP server
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter({caseSensitive: false, ignoreTrailingSlash: true}),
-    { bufferLogs: true
-    }
+    new FastifyAdapter({ caseSensitive: false, ignoreTrailingSlash: true }),
+    { bufferLogs: true }
   );
+
+  // Set up logging
   const logger = app.get(Logger);
   app.useLogger(logger);
   app.flushLogs();
+
+  // Enable compression for responses
   app.register(compress);
-  const isProduction = process.env.envirenment === 'production';
-  await app.getHttpAdapter().getInstance().register(fastifyCookie,{
-    parseOptions: {
-      secure: isProduction,
-    }});
+
+  // Determine if the application is running in production
+  const isProduction = process.env.environment === 'production';
+
+  // Set up cookie parsing
+  await app.getHttpAdapter().getInstance().register(fastifyCookie, {
+    parseOptions: { secure: isProduction }
+  });
+
+  // Enable CORS
   app.enableCors();
-  if(!isProduction) {
+
+  // Set up Swagger API documentation for non-production environments
+  if (!isProduction) {
     setupSwagger(app);
   }
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.useGlobalFilters(new AllExceptionsFilter());  
-  
-  const configService = app.get(ConfigService);
-  const serverConfig = configService.getOrThrow<ServerConfig>(ServerConfigName);
 
+  // Set up global pipes, filters, and interceptors
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(
     new LoggerErrorInterceptor(),
     new NewrelicInterceptor(),
     new ResponseInterceptor(),
   );
 
+  // Get server configuration
+  const configService = app.get(ConfigService);
+  const serverConfig = configService.getOrThrow<ServerConfig>(ServerConfigName);
+
   try {
-    await app.listen(serverConfig.port,serverConfig.host||'0.0.0.0');
+    await app.listen(serverConfig.port, serverConfig.host || '0.0.0.0');
     logger.log(
       `${process.env.APP_NAME || 'NestJs Scaffold'} Microservice Application is running on: ${await app.getUrl()}/api`,
-    );  } catch (error) {
+    );
+  } catch (error) {
     logger.error(`Error starting the application: ${error}`);
-  }  
-  
-
+  }
 }
+
+// Run the bootstrap function to start the application
 bootstrap();
